7a01ace4c4ca1ef8657be512574bd3e22fb4b760
Karthik <130748707+rapakakarthik@users.noreply.github.com>
2024-08-18 14:17:16 +0530

    Update Jenkinsfile.tf

--- Jenkinsfile.tf
+++ Jenkinsfile.tf
@@ -14,7 +14,7 @@ pipeline {
         }
         stage('deploy to tomcat') {
             steps {
-                deploy adapters: [tomcat9(credentialsId: 'TOMCAT', path: '', url: 'http://3.144.181.216:8081/')], contextPath: 'jan', war: 'target/*.war'
+                deploy adapters: [tomcat9(credentialsId: 'TOMCAT', path: '', url: 'http://3.144.181.216:8081/')], contextPath: 'script', war: 'target/*.war'
                 
             }
         }
